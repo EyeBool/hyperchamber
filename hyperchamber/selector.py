78698ca7c91d74a4753c05415709c0a038d7d84f
EyeBool <diataha.personal@gmail.com>
2018-06-03 22:57:11 -0700

    Added comments, and did slight refactoring and cleanup.

--- hyperchamber/selector.py
+++ hyperchamber/selector.py
@@ -9,121 +9,158 @@ from .config import Config
 
 # for function serialization
 class HCEncoder(JSONEncoder):
-  def default(self, o):
-    if(hasattr(o, '__call__')): # is function
-      return "function:" +o.__module__+"."+o.__name__
-    else:
-      try:
-          return o.__dict__    
-      except AttributeError:
-          try:
-             return str(o)
-          except AttributeError:
-              return super(o)
+    def default(self, o):
+        if hasattr(o, '__call__'):  # is function
+            return "function:" + o.__module__ + "." + o.__name__
+        else:
+            try:
+                return o.__dict__
+            except AttributeError:
+                try:
+                    return str(o)
+                except AttributeError:
+                    return super(o)
 
 
+# for enumerating possible hyperparameter value configurations (with each hyperparameter having possibly several values)
 class Selector:
-    def __init__(self, initialStore = {}):
-        self.store = initialStore
+    def __init__(self, initial_store={}):
+        """
+        Initialize a hyperparameter configuration selector.
+        """
+        self.store = initial_store
         self.results = []
+
     def set(self, key, value):
-        """Sets a hyperparameter.  Can be used to set an array of hyperparameters."""
-        self.store[key]=value
+        """
+        Set the value (or list of possible values) of a hyperparameter for the selector.
+
+        Can be used to set an array of hyperparameters at once.
+        """
+        self.store[key] = value
         return self.store
 
     def count_configs(self):
-        count = 1
+        """
+        Return the number of possible selections of hyperparameter configurations for the selector.
+        """
+        config_count = 1
 
         for key in self.store:
             value = self.store[key]
-            if(isinstance(value,list)):
-                count *= len(value)
+            if isinstance(value, list):
+                config_count *= len(value)
 
-        return count
+        return config_count
 
     def get_config_value(self, k, i):
-        """Gets the ith config value for k.  e.g. get_config_value('x', 1)"""
-        if(not isinstance(self.store[k], list)):
+        """
+        Gets the ith config value for k.  e.g. get_config_value('x', 1).
+        """
+        if not isinstance(self.store[k], list):
             return self.store[k]
         else:
             return self.store[k][i]
 
     def configs(self, max_configs=1, offset=None, serial=False, create_uuid=True):
-        """Generate max configs, each one a dictionary.  e.g. [{'x': 1}] 
+        """
+        Generate max configs, each one a dictionary.  e.g. [{'x': 1}]
 
           Will also add a config UUID, useful for tracking configs.  
           You can turn this off by passing create_uuid=False.
         """
-        if len(self.store)==0:
+        if len(self.store) == 0:
             return []
 
         configs = []
 
-        if(offset is None):
+        if offset is None:
             offset = max(0, random.randint(0, self.count_configs()))
         for i in range(max_configs):
             # get an element to index over
 
             config = self.config_at(offset)
-            if(create_uuid):
-              config["uuid"]=uuid.uuid4().hex
+            if create_uuid:
+                config["uuid"] = uuid.uuid4().hex
             configs.append(config)
-            if(serial):
-                offset+=1
+            if serial:
+                offset += 1
             else:
                 offset = max(0, random.randint(0, self.count_configs()))
         return configs
 
     def config_at(self, i):
-      """Gets the ith config"""
-      selections = {}
-      for key in self.store:
-        value = self.store[key]
-        if isinstance(value, list):
-            selected = i % len(value)
-            i = i // len(value)
-            selections[key]= value[selected]
-        else:
-            selections[key]= value
+        """
+        Go through all the possible selections of configurations, and return the ith selection.
+        """
+        selection = {}
+        for key in self.store:
+            value = self.store[key]
+            if isinstance(value, list):
+                selected = i % len(value)
+                i = i // len(value)
+                selection[key] = value[selected]
+            else:
+                selection[key] = value
 
-      return Config(selections)
+        return Config(selection)
 
     def random_config(self):
-      offset = max(0, random.randint(0, self.count_configs()))
-      return self.config_at(offset)
+        """
+        Get a random configuration selection.
+        """
+        offset = max(0, random.randint(0, self.count_configs()))
+        return self.config_at(offset)
 
     def reset(self):
-        """Reset the hyperchamber variables"""
+        """
+        Reset the hyperparameter configuration selector.
+        """
         self.store = {}
         self.results = []
         return
 
     def top(self, sort_by):
-        """Get the best results according to your custom sort method."""
+        """
+        Get the best results according to your custom sort method.
+        """
         sort = sorted(self.results, key=sort_by)
         return sort
 
     def record(self, config, result):
-        """Record the results of a config."""
+        """
+        Record the results of a config.
+        """
         self.results.append((config, result))
 
-    def load(self, filename):
-        """Loads a config from disk"""
+    # can be made into a function
+    @staticmethod
+    def load(filename):
+        """
+        Loads a configuration selector from disk.
+        """
         content = open(filename)
         return Config(json.load(content))
 
     def load_or_create_config(self, filename, config=None):
-        """Loads a config from disk.  Defaults to a random config if none is specified"""
+        """
+        Loads a config from disk.  Defaults to a random config if none is specified.
+        """
         os.makedirs(os.path.dirname(os.path.expanduser(filename)), exist_ok=True)
         if os.path.exists(filename):
-            return self.load(filename)
+            return Selector.load(filename)
 
-        if(config == None):
+        if config is None:
             config = self.random_config()
 
-        self.save(filename, config)
+        Selector.save(filename, config)
         return config
 
-    def save(self, filename, config):
-        """Loads a config from disk"""
-        return open(os.path.expanduser(filename), 'w').write(json.dumps(config, cls=HCEncoder, sort_keys=True, indent=2, separators=(',', ': ')))   
+    # can be made into a function
+    @staticmethod
+    def save(filename, config):
+        """
+        Save a configuration selector to disk.
+        """
+        with open(os.path.expanduser(filename), 'w') as file:
+            file.write(json.dumps(config, cls=HCEncoder, sort_keys=True, indent=2, separators=(',', ': ')))
